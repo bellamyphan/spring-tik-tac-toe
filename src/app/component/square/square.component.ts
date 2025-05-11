c14cf96ee8e964e0c46e08a55799011fd924a369
bellamyphan <bellamyphan@icloud.com>
2025-05-11 02:45:43 -0500

    feat: implement tic-tac-toe game with board and square components
    - Created BoardComponent to manage game state and player turns.
    - Added SquareComponent to represent individual squares on the board.
    - Implemented game logic including win condition checks and new game functionality.
    - Updated app.component.html to include the new BoardComponent.
    - Added styles for board and square components.
    - Created unit tests for both BoardComponent and SquareComponent.

---
+++ src/app/component/square/square.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  imports: [],
+  templateUrl: './square.component.html',
+  styleUrl: './square.component.css'
+})
+export class SquareComponent {
+
+  @Input() value!: 'X' | 'O';
+  
+}
